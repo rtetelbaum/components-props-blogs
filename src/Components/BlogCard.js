77ce4ad9872f1c7cfa2e582430cbbb868940fbdb
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-13 01:08:15 -0500

    added more details button

--- src/Components/BlogCard.js
+++ src/Components/BlogCard.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 class BlogCard extends React.Component {
 
-	handleClick = () => {
+	state = {
+		detailClicked: false
+	}
+
+	handleSaveDelete = () => {
 		if (this.props.saveBlog) {
 			this.props.saveBlog(this.props.blogObj)
 		} else if (this.props.deleteBlog) {
 			this.props.deleteBlog(this.props.blogObj.id)
+		}
+	}
+
+	handleDetail = () => {
+		this.setState(prevState => ({detailClicked: !prevState.detailClicked}))
+	}
+
+	renderMoreDetails = () => {
+		if (this.state.detailClicked) {
+			return <h4>By: {this.props.blogObj.author} </h4>
 		}
 	}
 
@@ -15,8 +29,9 @@ class BlogCard extends React.Component {
 			<>
 				<h5>{this.props.blogObj.title}</h5>
 				<img src={this.props.blogObj.image} alt="blog" style={{ maxWidth: "70vw", maxHeight: "20vh" }} /><br />
-				<button onClick={this.handleClick}>{this.props.saveBlog ? "Save" : "Delete"}</button><br />
-				<button>Visit</button>
+				{this.renderMoreDetails()}
+				<button onClick={this.handleDetail}>{this.state.detailClicked ? "Less Details" : "More Details"}</button><br />
+				<button onClick={this.handleSaveDelete}>{this.props.saveBlog ? "Save" : "Delete"}</button><br />
 			</>
 		)
 	}
--- src/Containers/BlogsList.js
+++ src/Containers/BlogsList.js
@@ -2,18 +2,16 @@ import React from 'react'
 import BlogCard from '../Components/BlogCard'
 import blogs from '../fakeAPI'
 
-class BlogsList extends React.Component {
+function BlogsList(props) {
 
-	blogsArray = blogs.map(blogObj => <BlogCard blogObj={blogObj} key={blogObj.id} saveBlog={this.props.saveBlog} />)
+	const blogsArray = blogs.map(blogObj => <BlogCard blogObj={blogObj} key={blogObj.id} saveBlog={props.saveBlog} />)
 	
-	render() {
 		return (
 			<>
 				<h1>Blogs List</h1>
-				{this.blogsArray}
+				{blogsArray}
 			</>
 		)
-	}
 
 }
 
